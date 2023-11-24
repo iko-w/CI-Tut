5b7b9c2b75b29f3887a659b96cd7300f303683d1
iko-w <iolubummo@sumo-digital.com>
2023-11-24 12:24:36 +0000

    Changes to script and added more lofgging information

--- TutorialStartPoint.py
+++ TutorialStartPoint.py
@@ -8,19 +8,30 @@ def RunBatchFile():
 	args = ['VS_Build.bat']
 	result = subprocess.run(args)
 
-def RunGitCommand():
+def RunGitCommand():	
 	# # Checking git status
 	statusResult = subprocess.run(['git', 'status'], capture_output=True, check=True)
 	print(statusResult.stdout.decode("ascii"))
+	file = open("gitstatusresult.txt", "w")
+	file.write("--Status--\n")
+	file.write(statusResult.stdout.decode("ascii"))
+	file.close()
 
-	# # cleaning git repo
-	cleanResult = subprocess.run(["git", "clean"], capture_output=True, check=True)
-	print(cleanResult.stdout.decode("ascii"))
+	# # getting head commit git repo
+	getHeadCommitResult = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True, check=True)
+	print(getHeadCommitResult.stdout.decode("ascii"))
+	file = open("headcommitresult.txt", "w")
+	file.write("--Commit being ran--\n")
+	file.write(getHeadCommitResult.stdout.decode("ascii"))
+	file.close()
 
 	# # pulling git repo
 	pullResult = subprocess.run(["git", "pull"], capture_output=True, check=True)
 	print(pullResult.stdout.decode("ascii"))
-
+	file = open("pullresult.txt", "w")
+	file.write("--Pull result--\n")
+	file.write(pullResult.stdout.decode("ascii"))
+	file.close()
 
 def PollForChanges():
 	print(time.ctime())
@@ -33,5 +44,5 @@ def RunForever():
 	while not ticker.wait(WAIT_TIME_SECONDS):
 		PollForChanges()
 
-# #PollForChanges()
-RunForever()
+PollForChanges()
+##RunForever()
